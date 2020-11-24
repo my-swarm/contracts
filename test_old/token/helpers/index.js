66706aba242759fcd40ee9c65ab97988712a593e
Jiri Blaha <blaha@wedecom.cz>
2020-11-24 18:17:18 +0100

    Got rid of all old tests and mocks ☀️

--- test_old/token/helpers/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  utils: require('./utils'),
-  accounts: require('./accounts'),
-  constants: require('./constants'),
-};
--- test_old/token/utils.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function encodeTransfer({address, amount}) {
-  return web3.utils.toHex(amount) + address.slice(2);
-}
-
-module.exports = {
-  encodeTransfer,
-};
