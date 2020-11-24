497739ed3de00f78252cf3518bda0781a8ad8c01
Jiri Blaha <blaha@wedecom.cz>
2020-11-24 18:23:20 +0100

    Deleted old deploy scripts

--- scripts/migrations/10_deploy_Utils.js
+++
@@ -1,5 +0,0 @@
-const utils = artifacts.require('Utils');
-
-module.exports = function (deployer) {
-  return deployer.deploy(utils).then(async (utils) => {});
-};
--- scripts/migrations/3_deploy_assetRegistry.js
+++
@@ -1,8 +0,0 @@
-const Factory = artifacts.require('SRC20Factory');
-const AssetRegistry = artifacts.require('AssetRegistry');
-
-module.exports = function (deployer) {
-  return Factory.deployed().then(async (factory) => {
-    return deployer.deploy(AssetRegistry, factory.address);
-  });
-};
