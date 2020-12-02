8a2791c86def90ebaf52382d0795704904d5245e
Jiri Blaha <blaha@wedecom.cz>
2020-12-02 10:58:21 +0100

    export fundraisermanager

--- scripts/export-helpers.js
+++ scripts/export-helpers.js
@@ -24,6 +24,7 @@ const BASE_CONTRACTS = {
   assetRegistry: 'AssetRegistry',
   tokenMinter: 'TokenMinter',
   affiliateManager: 'AffiliateManager',
+  fundraiserManager: 'FundraiserManager',
   usdc: 'USDC',
   disperse: 'Disperse',
 };
