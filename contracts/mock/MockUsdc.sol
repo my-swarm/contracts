0d5877debbdfe0c6ff7db6325795c4b8442016ea
Jiri Blaha <blaha@wedecom.cz>
2021-02-25 16:29:26 +0100

    Separated mock ERC20 contracts for better logging

--- contracts/mock/MockUsdc.sol
+++ contracts/mock/MockUsdc.sol
@@ -3,15 +3,11 @@ pragma solidity >=0.5.0 <0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract MockERC20 is ERC20 {
+contract MockUsdc is ERC20 {
   uint8 private _decimals;
 
-  constructor(
-    string memory name_,
-    string memory symbol_,
-    uint8 decimals_
-  ) public ERC20(name_, symbol_) {
-    _decimals = decimals_;
+  constructor() public ERC20('Mock USDC', 'USDC') {
+    _decimals = 6;
   }
 
   function mint(address account, uint256 amount) public {
