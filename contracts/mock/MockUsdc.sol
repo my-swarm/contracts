4222f49a543ac77d056098f98ba9255970f49856
Jiri Blaha <blaha@wedecom.cz>
2021-02-26 11:00:52 +0100

    Fixed mock USDC decimals setup

--- contracts/mock/MockUsdc.sol
+++ contracts/mock/MockUsdc.sol
@@ -4,10 +4,8 @@ pragma solidity >=0.5.0 <0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract MockUsdc is ERC20 {
-  uint8 private _decimals;
-
   constructor() ERC20('Mock USDC', 'USDC') {
-    _decimals = 6;
+    _setupDecimals(6);
   }
 
   function mint(address account, uint256 amount) public {
